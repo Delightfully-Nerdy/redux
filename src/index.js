682b9a3b60b49cc5f6a34268bba2c777b4d2de76
Dalitso Kasonde <dkasonde11@gmail.com>
2023-02-02 09:19:19 +0200

    create components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT
+
+// REDUCER 
+
+// DISPATCH 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
