7bfc92ee8e20a76830c4799a288a074ced24a75b
Dalitso Kasonde <dkasonde11@gmail.com>
2023-02-02 09:38:03 +0200

    add actions

--- src/index.js
+++ src/index.js
@@ -9,9 +9,38 @@ import {createStore} from 'redux';
 
 // ACTION INCREMENT
 
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
 // REDUCER 
 
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT' :
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// display in console 
+store.subscribe(() =>console.log(store.getState()));
+
 // DISPATCH 
+
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
